e8fc7861d64d2b4a3d6a52f096780437cc8a14ea
WebHookDev <114340252+WebHookDev@users.noreply.github.com>
2021-03-22 12:57:53 +0000

    Add or modify

--- PongANN/Assets/Scripts/Controller/AIController.cs
+++ PongANN/Assets/Scripts/Controller/AIController.cs
@@ -41,4 +41,4 @@ public class AIController : PaddleController
     {
         Ball instanceBall = GameMgr.Instance.Ball;
         m_ballPosY = instanceBall.transform.position.y;
-        m_ballDirectionX = i
+        m_ballDirectionX = instanceBall.R
