d64582c8ebfe868f274222d7502459a2410f0180
WebHookDev <114340252+WebHookDev@users.noreply.github.com>
2021-04-07 05:37:59 +0000

    Added tutorial

--- PongANN/Assets/Scripts/Controller/AIController.cs
+++ PongANN/Assets/Scripts/Controller/AIController.cs
@@ -47,4 +47,5 @@ public class AIController : PaddleController
         m_neuralNetwork.Execute( m_ballPosY, m_ballDirectionX, m_ballDirectionY );
 
         wantedPosY = Mathf.Lerp( -GameMgr.Instance.CourtHeight / 2.0f,
-                                 GameMgr.Instance.CourtHeight / 2.
+                                 GameMgr.Instance.CourtHeight / 2.0f,
+        
