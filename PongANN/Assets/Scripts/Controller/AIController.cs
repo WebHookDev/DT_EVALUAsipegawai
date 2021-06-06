ff7659f8bc3232aa65a7231f1eebc16eed2b0f6a
WebHookDev <114340252+WebHookDev@users.noreply.github.com>
2021-06-06 19:57:32 +0000

    fixed options

--- PongANN/Assets/Scripts/Controller/AIController.cs
+++ PongANN/Assets/Scripts/Controller/AIController.cs
@@ -80,4 +80,4 @@ public class AIController : PaddleController
 
     private void ComputeLearning( Vector3 ballPos )
     {
-        m_neuralNetwork.Execute( m_ballPosY, m_ballDirectionX, m_ballD
+        m_neuralNetwork.Execute( m_ballPosY, m_ballDirectionX, m_ballDirectio
