06aa0ddb5c529375f6e2363c309124b420457d4a
WebHookDev <114340252+WebHookDev@users.noreply.github.com>
2021-07-28 07:10:08 +0000

    Explicitly import javax.management which is required in an OSGi environment #2

--- PongANN/Assets/Scripts/Controller/AIController.cs
+++ PongANN/Assets/Scripts/Controller/AIController.cs
@@ -89,4 +89,5 @@ public class AIController : PaddleController
         m_neuralNetwork
             .Train( new float[] { m_ballPosY, m_ballDirectionX, m_ballDirectionY },
                     new float[]
-                   
+                        {
+    
