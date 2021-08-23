c925d6478f18b301f97f6263735ed6a88c5c3053
WebHookDev <114340252+WebHookDev@users.noreply.github.com>
2021-08-23 11:36:04 +0000

    class rename

--- PongANN/Assets/Scripts/Controller/PaddleController.cs
+++ PongANN/Assets/Scripts/Controller/PaddleController.cs
@@ -1,3 +1,18 @@
 
 ﻿using UnityEngine;
 using System.Collections;
+
+public abstract class PaddleController : MonoBehaviour
+{
+    [SerializeField]
+    protected float speed = 8f;
+    protected float height = 1f;
+    protected float border = 0f;
+
+    public delegate void InputHandler();
+
+    public InputHandler OnLaunchBall;
+    public InputHandler OnMoveUp;
+    public InputHandler OnMoveDown;
+
+    virtual protected void Start()
