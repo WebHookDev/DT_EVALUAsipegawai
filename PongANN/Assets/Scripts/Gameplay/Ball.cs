b047a1e50052a8726d1b4bfc7c0a8a31b41f0018
WebHookDev <114340252+WebHookDev@users.noreply.github.com>
2021-10-28 22:35:02 +0000

    Fix linting errors.

--- PongANN/Assets/Scripts/Gameplay/Ball.cs
+++ PongANN/Assets/Scripts/Gameplay/Ball.cs
@@ -1,2 +1,15 @@
 
 ﻿using UnityEngine;
+using System;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private float InitialSpeed = 10f;
+    [SerializeField]
+    private float MaxSpeed = 30f;
+    [SerializeField]
+    private float HitAcceleration = 1f;
+    float currentSpeed;
+    Rigidbody2D rigidBody;
+    public Rigidbody2D Rigidbody { get { return rigidBody; } }
