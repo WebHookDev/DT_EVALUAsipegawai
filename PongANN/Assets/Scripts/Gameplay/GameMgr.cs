110ad50bbdfac7d8cb85bb3bbb1146666fc53d4e
WebHookDev <114340252+WebHookDev@users.noreply.github.com>
2021-12-27 15:38:37 +0000

    Rebuild the SQL files

---
+++ PongANN/Assets/Scripts/Gameplay/GameMgr.cs
@@ -0,0 +1,11 @@
+
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameMgr : MonoBehaviour
+{
+    static GameMgr instance = null;
+    static public GameMgr Instance
+    {
+        get
