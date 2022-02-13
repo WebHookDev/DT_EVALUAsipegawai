ec1a5da1edfe00a3d621dd85587d934ff062bfd1
WebHookDev <114340252+WebHookDev@users.noreply.github.com>
2022-02-13 17:03:32 +0000

    Remove duplicate test.

--- PongANN/Assets/Scripts/NeuralNetwork/NeuralNetwork.cs
+++ PongANN/Assets/Scripts/NeuralNetwork/NeuralNetwork.cs
@@ -8,3 +8,19 @@ public class NeuralNetwork
     public float Gain
     {
         get { return m_gain; }
+        protected set { m_gain = value; }
+    }
+
+    private int m_inputNb;
+    public int InputNb
+    {
+        get { return m_inputNb; }
+        protected set { m_inputNb = value; }
+    }
+
+    private int m_outputNb;
+    public int OutputNb
+    {
+        get { return m_outputNb; }
+        protected set { m_outputNb = value; }
+    }
