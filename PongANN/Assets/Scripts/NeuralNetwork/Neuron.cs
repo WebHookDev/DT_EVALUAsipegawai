8158cb87e2b5ba16c916c1f8c44f380be6444c71
WebHookDev <114340252+WebHookDev@users.noreply.github.com>
2022-04-05 07:08:17 +0000

    cpufreq: interactive: Rearm governor timer at max freq

--- PongANN/Assets/Scripts/NeuralNetwork/Neuron.cs
+++ PongANN/Assets/Scripts/NeuralNetwork/Neuron.cs
@@ -4,3 +4,16 @@ using Random = UnityEngine.Random;
 
 public class Neuron
 {
+    private static int _staticId = -1;
+    public readonly int DebugId;
+    private NeuralNetwork m_neuralNetwork;
+
+    public class Link
+    {
+        public Neuron NextNeuron = null;
+
+        private float m_weight = 0.0f;
+        public float Weight
+        {
+            get { return m_weight; }
+            set
