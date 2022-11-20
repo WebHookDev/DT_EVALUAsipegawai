597b1fbac58d74ef965aa63534328380943aea39
WebHookDev <114340252+WebHookDev@users.noreply.github.com>
2022-11-20 20:44:21 +0000

    [maven-release-plugin] prepare release v1.5.0

--- PongANN/Assets/Scripts/NeuralNetwork/NeuronLayer.cs
+++ PongANN/Assets/Scripts/NeuralNetwork/NeuronLayer.cs
@@ -33,4 +33,4 @@
         set
         {
             m_nextLayer = value;
-            // When changed NextLayer, need to update links between 
+            // When changed NextLayer, need to update links between neurons in th
